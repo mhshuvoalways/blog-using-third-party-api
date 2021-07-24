383ac82d1886c5afb8070d10f80271726b9e1e69
mhshuvoit <mhshuvoit@gmail.com>
2021-07-24 22:43:01 +0600

    still tring to solve cors policy

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,11 @@
 import Axios from 'axios';
 
+const PROXY = window.location.hostname === "localhost"
+    ? "https://cors-anywhere.herokuapp.com"
+    : "/cors-proxy";
+
 const intence = Axios.create({
-    baseURL: process.env.REACT_APP_NEWS_URL,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    'mode': 'cors',
+    baseURL: PROXY + process.env.REACT_APP_NEWS_URL
 })
 
 intence.defaults.headers.common['X-Api-Key'] = process.env.REACT_APP_API_KEY
