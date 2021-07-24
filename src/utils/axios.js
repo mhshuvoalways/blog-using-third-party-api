cc0f5143b708e049a0407d73d29c695fcfe3493e
mhshuvoit <mhshuvoit@gmail.com>
2021-07-24 20:43:24 +0600

    trying to solve cors policy issue

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 const intence = Axios.create({
-    baseURL: process.env.REACT_APP_NEWS_URL
+    baseURL: process.env.REACT_APP_NEWS_URL,
+    headers: { "Access-Control-Allow-Origin": "*" }
 })
 
 intence.defaults.headers.common['X-Api-Key'] = process.env.REACT_APP_API_KEY
