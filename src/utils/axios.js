74bfa27115ecab508f2f0f7196bcaeca16e3b398
mhshuvoit <mhshuvoit@gmail.com>
2021-07-24 22:25:45 +0600

    mode: cors

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,8 @@ import Axios from 'axios';
 
 const intence = Axios.create({
     baseURL: process.env.REACT_APP_NEWS_URL,
-    headers: { "Access-Control-Allow-Origin": "*" }
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    'mode': 'cors',
 })
 
 intence.defaults.headers.common['X-Api-Key'] = process.env.REACT_APP_API_KEY
