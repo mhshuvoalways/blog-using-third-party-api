70ecbd381b5f1cd5cb935f1918c14fa035bfd01a
mhshuvoit <mhshuvoit@gmail.com>
2021-07-25 09:07:32 +0600

    heroku proxy add

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const intence = Axios.create({
-    baseURL: process.env.REACT_APP_NEWS_URL
+    baseURL: `https://cors-anywhere.herokuapp.com/${process.env.REACT_APP_NEWS_URL}`
 })
 
 intence.defaults.headers.common['X-Api-Key'] = process.env.REACT_APP_API_KEY
