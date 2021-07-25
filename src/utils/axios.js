264887ed3baa3204dfba3ef2526aafd2e61ebbd5
mhshuvoit <mhshuvoit@gmail.com>
2021-07-25 09:14:14 +0600

    proxy add again

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,11 @@
 import Axios from 'axios';
 
+const PROXY = window.location.hostname === "localhost"
+    ? "https://cors-anywhere.herokuapp.com"
+    : "/cors-proxy";
+
 const intence = Axios.create({
-    baseURL: `https://cors-anywhere.herokuapp.com/${process.env.REACT_APP_NEWS_URL}`
+    baseURL: `${PROXY}/${process.env.REACT_APP_NEWS_URL}`
 })
 
 intence.defaults.headers.common['X-Api-Key'] = process.env.REACT_APP_API_KEY
