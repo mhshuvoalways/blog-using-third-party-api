0199e56ee71dce6bd32fc6c5269ade47eb6aeb1a
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-08 17:11:02 +0600

    path install

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.get("/api/:url", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "./client", "build")));
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "./client", "build", "index.html"),
+    path.resolve(__dirname, "client", "build", "index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
