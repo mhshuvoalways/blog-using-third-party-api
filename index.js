377fa8dc486d4b1626871717ef42d9b6b39f17c1
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-08 16:44:13 +0600

    change

--- index.js
+++ index.js
@@ -17,10 +17,19 @@ app.get("/api/:url", async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "client", "build")));
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
