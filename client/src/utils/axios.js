afa20e0606acdc80d2ae008684780b098b9b08a0
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-08 16:24:55 +0600

    change

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
-import Axios from 'axios';
+import Axios from "axios";
 
 const intence = Axios.create({
-    baseURL: `http://localhost:5000`
-})
+  baseURL: "http://localhost:5000",
+});
 
-export default intence
+export default intence;
