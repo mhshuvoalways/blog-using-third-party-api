67ad730ba8bbdb93948ff610ef81c2f13d8d7d6c
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-08 16:28:40 +0600

    chage

--- utils/axios.js
+++ utils/axios.js
@@ -1,9 +1,9 @@
 const Axios = require('axios')
 
 const intence = Axios.create({
-    baseURL: `https://newsapi.org/v2/top-headlines`
+    baseURL: `${process.env.NEWS_URL}`
 })
 
-intence.defaults.headers.common['X-Api-Key'] = 'c18c9dd49af0488b91e0e27f1c552fd5'
+intence.defaults.headers.common['X-Api-Key'] = process.env.API_KEY
 
 module.exports = intence
