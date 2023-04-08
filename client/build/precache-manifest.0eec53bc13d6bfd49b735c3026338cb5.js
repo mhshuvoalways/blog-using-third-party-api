7483c1176418d227cba578476c315cd6c956f494
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-08 17:15:58 +0600

    build add

---
+++ client/build/precache-manifest.0eec53bc13d6bfd49b735c3026338cb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eacfbf5432f2db39f10e0ab3bd4d7e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18887729332995fc4697",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ad4658e158173a2b6fc2",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "18887729332995fc4697",
+    "url": "/static/js/2.83d8250a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.83d8250a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4658e158173a2b6fc2",
+    "url": "/static/js/main.4349e79e.chunk.js"
+  },
+  {
+    "revision": "cef7d221a72db50095ef",
+    "url": "/static/js/runtime-main.f956e909.js"
+  }
+]);
--- index.js
+++ index.js
@@ -17,12 +17,17 @@ app.get("/api/:url", async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client", "build")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.resolve(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "client", "build", "index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
