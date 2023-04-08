56ae7ff4744aedf824d7c5e066bda324843416dd
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-08 16:34:43 +0600

    change

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,12 @@
 import Axios from "axios";
 
+let baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://blog-using-third-party-api.vercel.app"
+    : "http://localhost:5000";
+
 const intence = Axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL,
 });
 
 export default intence;
