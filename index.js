3ae44fc3061666de202b326baaf85a47155fd9d5
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-08 16:55:15 +0600

    testing

--- index.js
+++ index.js
@@ -17,19 +17,17 @@ app.get("/api/:url", async (req, res) => {
   }
 });
 
-
-  app.use(express.static(path.resolve(__dirname, "./client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "./client", "build", "index.html"),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
+app.use(express.static(path.resolve(__dirname, "./client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "./client", "build", "index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
       }
-    );
-  });
-
+    }
+  );
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
